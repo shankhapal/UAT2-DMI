37f986fc032360df2d8d274b63714e9227560814
Amol Choudhari <amy.cho27@gmail.com>
2023-05-26 18:44:11 +0530

    updated grant certificates pdfs with condition on change fields element call

--- templates/Applicationformspdfs/grantCaCertificatePdf.php
+++ templates/Applicationformspdfs/grantCaCertificatePdf.php
@@ -475,7 +475,12 @@
 				
 							
 				<!-- element call to show change updates added on 02-01-2023-->
-				<?php echo $this->element('application_forms/change/elementChangeUpdatesOnCertificate'); ?>
+				<?php
+					//condition added on 26-05-2023, to get changed details only when appl. is for change or already changed earlier 
+					if($_SESSION['application_type']==3 || !empty($getNoOfAppl)){
+						echo $this->element('application_forms/change/elementChangeUpdatesOnCertificate');
+					}
+				 ?>
 						
 		
 		</table>
