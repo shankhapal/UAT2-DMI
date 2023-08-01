40f3e8fcbcb2d86adaaaff1265ea4b1c3320cf0a
unknown <bhadadelaxmi@gmail.com>
2023-08-01 16:51:51 +0530

    experice cal done

--- webroot/js/custom.validation.v.5.js
+++ webroot/js/custom.validation.v.5.js
@@ -10,21 +10,46 @@ $(function(){
 		var date1 = $("#ta-from_dt-"+id_No).val();
 		var date2 = $("#ta-to_dt-"+id_No).val(); 
 		var yearsDiff =  new Date(date2).getFullYear() - new Date(date1).getFullYear();
-    //calculated experience difference with basis  of month and year by laxmi 10-07-2023
-     const split = date1.split('/');
-     const split1 = date2.split('/');
-     var monthsDiff = split1[1]-split[1];
-     console.log((new Date(split1[1]+'/'+split1[0]+'/'+split1[2]).getMonth()+1)-(new Date(split[1]+'/'+split[0]+'/'+split[2]).getMonth()+1));
+    //calculated experience difference with basis  of month and year by laxmi 01-08-2023
+      const split = date1.split('/');
+      const split1 = date2.split('/');
+     
+      const start = new Date(split[2]+','+split[1]);
+      const end = new Date(split1[2]+','+split1[1]);
+      var month= end.getMonth() - start.getMonth() + 
+        (12 * (end.getFullYear() - start.getFullYear()));
+
+        if(month == 12 || month > 12){
+          if(month == 12){
+            
+            yearsDiff = 1;
+            monthsDiff = 0;
+          }else if(month > 12){
+            var i = 10;
+            var calMonth = 12;
+            var calculated_years;
+            var calculatedMonth;
+            for (var k = 1; k<=i; k++){
+              calculated_years = month / calMonth;
+              calculatedMonth =  month % calMonth;
+            }
+            yearsDiff = Math.trunc(calculated_years);
+            monthsDiff = calculatedMonth;
+          }else{
+            yearsDiff = 0;
+            monthsDiff = month;
+          }    
+        }
     
-     if(yearsDiff === 0 || yearsDiff === NaN){
-      yearsDiff =  0;
-     }
-     if(monthsDiff === 0 || monthsDiff === NaN){
-      monthsDiff =  0;
-     }
+        if(yearsDiff === 0 || yearsDiff === NaN){
+          yearsDiff =  0;
+        }
+        if(monthsDiff === 0 || monthsDiff === NaN){
+          monthsDiff =  0;
+        }
 		
-    $("#ta-total-"+id_No).val(yearsDiff +"."+ monthsDiff);
-    })
+      $("#ta-total-"+id_No).val(yearsDiff +"."+ monthsDiff);
+      })
   
 	$('#value1, #value2').keyup(function(){
 
